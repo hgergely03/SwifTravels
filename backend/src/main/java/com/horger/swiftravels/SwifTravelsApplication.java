5273a5e7d583eb8b668e15c852db1e62cf822e0b
Horváth Gergely <gergely0703@gmail.com>
2022-01-26 20:22:08 +0100

    feat(backend): create base project

---
+++ backend/src/main/java/com/horger/swiftravels/SwifTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.horger.swiftravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwifTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwifTravelsApplication.class, args);
+	}
+
+}
